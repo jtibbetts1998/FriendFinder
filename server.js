acfbf5cb58dc55eb3f74621e5a827b4d871ca3d2
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-19 09:33:21 -0500

    html and logic update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("app/public/"));
 app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
