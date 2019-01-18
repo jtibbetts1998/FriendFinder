c78ea4e6a3bd47f54d09b171d47a0de2d8108d65
jtibbetts1998 <justin.tibbetts98@gmail.com>
2019-01-18 14:31:01 -0500

    linking home to survey

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("app/public/"));
+//app.use(express.static("app/public/"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT: http://localhost:" + PORT);
+    console.log("App listening on PORT: " + PORT);
 });
 
