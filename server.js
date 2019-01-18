278121fe51c5f3e4a0fa694f27b46c33fb892c9a
jtibbetts1998 <justin.tibbetts98@gmail.com>
2019-01-18 13:46:40 -0500

    adding logic to take in value of user answers

--- server.js
+++ server.js
@@ -16,5 +16,6 @@ require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: http://localhost:" + PORT);
 });
+
