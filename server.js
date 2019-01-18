3b9862d92f77d09cb1324048c5afa3590033fc66
jtibbetts1998 <justin.tibbetts98@gmail.com>
2019-01-18 14:07:29 -0500

    file structure

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static("app/public/"));
 app.use(express.json());
+app.use(express.static("app/public/"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
